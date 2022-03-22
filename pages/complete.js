768a1000589532f8652b57473566ae0160cd8ce3
Amir Iskandar <iskandar.amir@gmail.com>
2022-03-22 09:45:52 +0800

    Success page complete

--- pages/complete.js
+++ pages/complete.js
@@ -1,3 +1,9 @@
 export default function complete() {
-  return <div></div>;
+  return (
+    <div className="flex h-screen">
+      <div className="m-auto text-xl font-bold underline text-center">
+        Your request has been submitted successfully!
+      </div>
+    </div>
+  );
 }
